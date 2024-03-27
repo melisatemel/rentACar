b04647738a04ce7fd97488642fb007189ccb6160
Melisa Temel <melisatml2501@gmail.com>
2024-03-27 21:11:39 +0300

    init

---
+++ src/main/java/com/etiya/rentACar/business/abstracts/CityService.java
@@ -0,0 +1,16 @@
+package com.etiya.rentACar.business.abstracts;
+
+import com.etiya.rentACar.business.dtos.requests.city.CreateCityRequest;
+import com.etiya.rentACar.business.dtos.requests.city.UpdateCityRequest;
+import com.etiya.rentACar.business.dtos.responses.city.*;
+
+
+import java.util.List;
+
+public interface CityService {
+    CreatedCityResponse add(CreateCityRequest createCityRequest);
+    UpdatedCityResponse update(UpdateCityRequest updateCityRequest);
+    List<GetCityListResponse> getAll();
+    GetCityResponse getById(int id);
+    void delete(int id);
+}
---
+++ src/main/java/com/etiya/rentACar/business/rules/CityBusinessRules.java
@@ -0,0 +1,25 @@
+package com.etiya.rentACar.business.rules;
+
+import com.etiya.rentACar.core.exceptions.types.BusinessException;
+import com.etiya.rentACar.dataAccess.abstracts.CityRepository;
+import com.etiya.rentACar.entities.Brand;
+import com.etiya.rentACar.entities.City;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class CityBusinessRules {
+    private CityRepository cityRepository;
+
+    public void cityNAmeCannotBeDuplicated(String cityName){
+        Optional<City> city = cityRepository.findByNameIgnoreCase(cityName.trim());
+
+        if(city.isPresent()){
+            throw new BusinessException("City Exists!");
+        }
+    }
+
+}
